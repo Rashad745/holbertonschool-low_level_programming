9990739c7bcb0cbd95e2d83042269a139eba8ab3
Rashad745 <rnuraliyev@gmail.com>
2025-12-06 10:16:05 +0000

    Add 1-last_digit.c Holberton assignment

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 /**
- * main - assigns a random number to n and prints the last digit with a message
+ * main - Prints the last digit of a randomly generated number
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,11 +16,14 @@ int main(void)
 	last_digit = n % 10;
 
 	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n",
+		       n, last_digit);
 	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n",
+		       n, last_digit);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		       n, last_digit);
 
 	return (0);
 }
