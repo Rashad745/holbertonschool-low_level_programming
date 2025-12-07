4f17aadd7894d8d571257a63c56bd1bbc40760fe
Rashad745 <rnuraliyev@gmail.com>
2025-12-07 10:05:10 +0000

    task zero

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * main - prints _putchar
+ * Return: 0
  */
 int main(void)
 {
-	char word[] = "_putchar";
-	int i = 0;
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-	while (word[i] != '\0')
-	{
-		_putchar(word[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
+    return (0);
 }
+
--- functions_nested_loops/functions_nested_loops/README.md
+++
@@ -1,8 +0,0 @@
-# Functions and Nested Loops
-
-This project contains the tasks for Holberton School (ALX) focusing on functions, nested loops, and header files in C.
-
-Files included:
-- 0-putchar.c
-- main.h
-
