44893533cc025909a5abb594e07a858a1f6325ca
Rashad745 <rnuraliyev@gmail.com>
2025-12-09 11:45:21 +0000

    file3

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,19 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: pointer to the string to check
+ * @s: pointer to the string
  *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    return (length);
+	return (length);
 }
+
