d43758b229528e0e2b8864872c2844db90f862a3
Rashad745 <rnuraliyev@gmail.com>
2025-12-09 13:46:40 +0000

    update

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,32 @@
 #include "main.h"
 
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to process
+ *
+ * Return: void
+ */
 void puts2(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	if (!str)
-		return;
+    /* Check if string is empty */
+    if (str == NULL)
+        return;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i += 2;
-	}
-
-	_putchar('\n');
+    /* Loop through the string */
+    while (str[i] != '\0')
+    {
+        /* Print current character */
+        _putchar(str[i]);
+        
+        /* Move to next character to print (skip one) */
+        i += 2;
+        
+        /* Check if we've reached or passed the end */
+        if (str[i - 1] == '\0')
+            break;
+    }
+    
+    _putchar('\n');
 }
-
