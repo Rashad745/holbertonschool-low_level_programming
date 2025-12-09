94bd46e8404cb2eb9ec99a76dcfcbcbeaa2d84e4
Rashad745 <rnuraliyev@gmail.com>
2025-12-09 14:02:08 +0000

    file8

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to process
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+    int len = 0;
+    int i;
+
+    while (str[len] != '\0')
+        len++;
+
+    if (len % 2 == 0)
+        i = len / 2;
+    else
+        i = (len + 1) / 2;
+
+    for (; i < len; i++)
+        _putchar(str[i]);
+
+    _putchar('\n');
+}
