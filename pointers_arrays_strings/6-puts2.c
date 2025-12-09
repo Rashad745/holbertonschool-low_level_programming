13a49c8fc3fe7d227fbfe45f11e5af66d984c42c
Rashad745 <rnuraliyev@gmail.com>
2025-12-09 13:48:55 +0000

    update

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,23 +10,15 @@ void puts2(char *str)
 {
     int i = 0;
 
-    /* Check if string is empty */
-    if (str == NULL)
-        return;
-
     /* Loop through the string */
     while (str[i] != '\0')
     {
-        /* Print current character */
         _putchar(str[i]);
-        
         /* Move to next character to print (skip one) */
         i += 2;
-        
-        /* Check if we've reached or passed the end */
+        /* Check if we've reached the end of string */
         if (str[i - 1] == '\0')
             break;
     }
-    
     _putchar('\n');
 }
