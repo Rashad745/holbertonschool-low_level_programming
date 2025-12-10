51a196265f5de231dc95fe8f0fade04ce6129ea9
Rashad745 <rnuraliyev@gmail.com>
2025-12-10 10:29:25 +0000

    filen3

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,16 +9,22 @@
 int _atoi(char *s)
 {
 	int i = 0, sign = 1, num = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			/* Check for potential overflow before multiplying */
+			if (num > (2147483647 - (s[i] - '0')) / 10)
+			{
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
 			num = num * 10 + (s[i] - '0');
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
 		}
 		else if (num > 0)
 			break;
@@ -26,3 +32,4 @@ int _atoi(char *s)
 	}
 	return (num * sign);
 }
+
