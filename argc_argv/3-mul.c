dffc332b7fb4f5b9a55ec6a27edd17d440ae50a0
Rashad745 <rnuraliyev@gmail.com>
2025-12-15 12:03:50 +0000

    4-add.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,18 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-  int a, b, result;
+	int a, b, result;
 
-  if (argc != 3)
-  {
-    printf("Error\n");
-    return (1);
-  }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-  a = atoi(argv[1]);
-  b = atoi(argv[2]);
-  result = a * b;
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
 
-  printf("%d\n", result);
-  return (0);
+	printf("%d\n", result);
+	return (0);
 }
