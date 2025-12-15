82a48caa677935b4381c23da2600f8bb5716c295
Rashad745 <rnuraliyev@gmail.com>
2025-12-15 11:34:32 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,3 @@
-#include <stdio.h>
-
-/**
- * main - prints the name of the program
- * @argc: argument count
- * @argv: argument vector (array of strings)
- *
- * Return: 0 on success
- */
 int main(int argc, char *argv[])
 {
   (void)argc; /* argc is unused */
