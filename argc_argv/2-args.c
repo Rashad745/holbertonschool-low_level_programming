e64d50414e805605a2ad2ab9584e21dd8f762f90
Rashad745 <rnuraliyev@gmail.com>
2025-12-15 12:07:30 +0000

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -8,8 +8,8 @@
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
-{ int i;
-  for (i = 0; i < argc; i++)
+{int i;
+	for (i = 0; i < argc; i++)
   { printf("%s\n", argv[i]);
   }
   return (0);
