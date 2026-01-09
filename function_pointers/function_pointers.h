e98ad4db6d7e1913c1b795948328f5ea1cea65df
Rashad745 <rnuraliyev@gmail.com>
2026-01-09 13:12:57 +0000

    file3

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,8 +3,14 @@
 
 #include <stddef.h>
 
-/* Prototype for your array_iterator function */
+/* Task 0 */
+void print_name(char *name, void (*f)(char *));
+
+/* Task 1 */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* Task 2 */
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
 
