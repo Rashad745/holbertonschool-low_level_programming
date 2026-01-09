d8e3fe0a793bb6272ca984d73220521f42d34c87
Rashad745 <rnuraliyev@gmail.com>
2026-01-09 10:26:56 +0000

    function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,11 +3,9 @@
 
 #include <stdio.h>
 
-/* Function prototype for the main task */
 void print_name(char *name, void (*f)(char *));
-
-/* Optional helper functions from your main.c */
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 
 #endif /* FUNCTION_POINTERS_H */
+
