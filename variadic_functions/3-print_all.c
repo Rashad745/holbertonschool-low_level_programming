9d887afe1f9e0352ec6cdbf1414b37ad249794ab
Rashad745 <rnuraliyev@gmail.com>
2026-01-12 07:14:18 +0000

    file3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,50 +1,70 @@
 #include "variadic_functions.h"
 
+typedef void (*printer)(va_list args);
+
+void print_char(va_list args)
+{
+    printf("%c", va_arg(args, int));
+}
+
+void print_int(va_list args)
+{
+    printf("%d", va_arg(args, int));
+}
+
+void print_float(va_list args)
+{
+    printf("%f", va_arg(args, double));
+}
+
+void print_string(va_list args)
+{
+    char *s = va_arg(args, char *);
+    if (s == NULL)
+        s = "(nil)";
+    printf("%s", s);
+}
+
 /**
  * print_all - prints anything
- * @format: list of types of arguments
  */
 void print_all(const char * const format, ...)
 {
     va_list args;
     unsigned int i = 0;
-    char *s;
     char *sep = "";
+    typedef struct {
+        char symbol;
+        printer f;
+    } mapping;
+
+    mapping map[] = {
+        {'c', print_char},
+        {'i', print_int},
+        {'f', print_float},
+        {'s', print_string},
+        {0, NULL}
+    };
 
     va_start(args, format);
 
     while (format && format[i])
     {
-        while (format[i] != 'c' && format[i] != 'i' &&
-               format[i] != 'f' && format[i] != 's' && format[i])
-            i++;
-
-        if (format[i])
+        unsigned int j = 0;
+        while (map[j].symbol)
         {
-            printf("%s", sep);
-
-            if (format[i] == 'c')
-                printf("%c", va_arg(args, int));
-
-            if (format[i] == 'i')
-                printf("%d", va_arg(args, int));
-
-            if (format[i] == 'f')
-                printf("%f", va_arg(args, double));
-
-            if (format[i] == 's')
+            if (map[j].symbol == format[i])
             {
-                s = va_arg(args, char *);
-                if (s == NULL)
-                    s = "(nil)";
-                printf("%s", s);
+                printf("%s", sep);
+                map[j].f(args);
+                sep = ", ";
             }
-
-            sep = ", ";
+            j++;
         }
         i++;
     }
 
-    printf("\n");
     va_end(args);
+    printf("\n");
 }
+
