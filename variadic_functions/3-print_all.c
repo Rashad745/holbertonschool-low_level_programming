12383589a08f50425560c2fde03a0162dc08110e
Rashad745 <rnuraliyev@gmail.com>
2026-01-12 10:58:57 +0000

    file4

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -18,46 +18,56 @@ void print_all(const char * const format, ...)
 	unsigned int i = 0, j;
 	const char *sep = "";
 	const char types[] = {'c', 'i', 'f', 's'};
-	void (*funcs[])(va_list) = {print_char, print_int, print_float, print_string};
+	void (*funcs[])(va_list) = {
+		print_char, print_int, print_float, print_string
+	};
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-j = 0;
-while (j < 4)
-{
-if (format[i] == types[j])
-{
-printf("%s", sep);
-funcs[j](args);
-sep = ", ";
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == types[j])
+			{
+				printf("%s", sep);
+				funcs[j](args);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
 }
-j++;
-}
-i++;
-}
-va_end(args);
-printf("\n");
-}
+
 /* Helper functions definitions */
+
 void print_char(va_list args)
 {
-printf("%c", va_arg(args, int));
+	printf("%c", va_arg(args, int));
 }
+
 void print_int(va_list args)
 {
-printf("%d", va_arg(args, int));
+	printf("%d", va_arg(args, int));
 }
+
 void print_float(va_list args)
 {
-printf("%f", va_arg(args, double));
+	printf("%f", va_arg(args, double));
 }
+
 void print_string(va_list args)
 {
-char *s = va_arg(args, char *);
-if (!s)
-s = "(nil)";
-printf("%s", s);
+	char *s = va_arg(args, char *);
+
+	if (!s)
+		s = "(nil)";
+
+	printf("%s", s);
 }
 
