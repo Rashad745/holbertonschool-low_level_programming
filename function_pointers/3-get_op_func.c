adb79ca2fbc51332449f933d085c4812715d1948
Rashad745 <rnuraliyev@gmail.com>
2026-01-12 06:00:13 +0000

    file4

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,29 +1,30 @@
 #include "3-calc.h"
+#include <string.h>
 
 /**
- * get_op_func - selects the correct function
- * @s: operator passed as argument
+ * get_op_func - selects the correct function to perform the operation
+ * @s: operator
  *
- * Return: pointer to function or NULL
+ * Return: pointer to the function or NULL
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
 
-	while (ops[i].op)
-	{
-		if (*(ops[i].op) == *s && s[1] == '\0')
-			return (ops[i].f);
-		i++;
-	}
-
-	return (NULL);
+    while (ops[i].op != NULL)
+    {
+        if (strcmp(ops[i].op, s) == 0)
+            return (ops[i].f);
+        i++;
+    }
+    return (NULL);
 }
+
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,42 +1,38 @@
 #include "3-calc.h"
 
 /**
- * main - performs simple arithmetic operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success, exits with error code otherwise
+ * main - entry point
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*f)(int, int);
+    int a, b, result;
+    int (*f)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	f = get_op_func(argv[2]);
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
 
-	if (f == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    f = get_op_func(argv[2]);
+    if (f == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
 
-	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+    result = f(a, b);
+    printf("%d\n", result);
 
-	printf("%d\n", f(a, b));
-
-	return (0);
+    return (0);
 }
 
