4cab1654c23f27df17affba2e4cd71d10856bab5
Rashad745 <rnuraliyev@gmail.com>
2026-02-05 17:38:55 +0000

    file0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,18 +9,16 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-
-    while (h)
-    {
-        if (h->str)
-            printf("[%u] %s\n", h->len, h->str);
-        else
-            printf("[0] (nil)\n");
-
-        count++;
-        h = h->next;
-    }
-
-    return (count);
+size_t count = 0;
+ while (h)
+{
+if (h->str)
+printf("[%u] %s\n", h->len, h->str);
+else
+printf("[0] (nil)\n");
+count++;
+h = h->next;
 }
+return (count);
+}
+
