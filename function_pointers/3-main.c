70ddf3e52669db3b35eac31b5bd997767a3868f2
Rashad745 <rnuraliyev@gmail.com>
2026-02-19 08:46:20 +0000

    file3func

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,31 +1,33 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-* main - entry point
-*/
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-int a, b, result;
-int (*f)(int, int);
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-f = get_op_func(argv[2]);
-if (f == NULL
-{
-printf("Error\n");
-exit(99);
-}
-if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
-{
-printf("Error\n");
-exit(100);
-}
-result = f(a, b);
-printf("%d\n", result);
-return (0);
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	f = get_op_func(argv[2]);
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
 
