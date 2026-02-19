c4cd84558b33e2ee86f92c4dfbf83cf71089bc8c
Rashad745 <rnuraliyev@gmail.com>
2026-02-19 08:42:57 +0000

    file3func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
 #include <string.h>
-
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: operator
@@ -9,7 +8,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
+op_t ops[] = {
         {"+", op_add},
         {"-", op_sub},
         {"*", op_mul},
@@ -17,14 +16,13 @@ int (*get_op_func(char *s))(int, int)
         {"%", op_mod},
         {NULL, NULL}
     };
-    int i = 0;
-
-    while (ops[i].op != NULL)
+int i = 0;
+while (ops[i].op != NULL)
     {
-        if (strcmp(ops[i].op, s) == 0)
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+if (strcmp(ops[i].op, s) == 0)
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
 
