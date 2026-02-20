936e92e2c7ce0dc33664ce24cd4ff846de8993c7
Rashad745 <rnuraliyev@gmail.com>
2026-02-20 06:51:53 +0000

    file3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,64 +1,58 @@
 #include "hash_tables.h"
-#include <string.h>  /* For strcmp */
-#include <stdlib.h>  /* For malloc, free, strdup */
 
 /**
  * hash_table_set - Adds or updates an element in the hash table
  * @ht: The hash table
  * @key: The key (cannot be empty)
- * @value: The value (must be duplicated, can be empty string)
+ * @value: The value associated with the key
  *
  * Return: 1 if success, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *tmp;
+	hash_node_t *new_node, *tmp;
+	unsigned long int idx;
 
-    if (!ht || !key || *key == '\0' || !value)
-        return (0);
+	if (!ht || !key || !*key || !value)
+		return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    tmp = ht->array[index];
+	idx = key_index((const unsigned char *)key, ht->size);
 
-    /* Check if key exists and update value */
-    while (tmp)
-    {
-        if (strcmp(tmp->key, key) == 0)
-        {
-            char *new_val = strdup(value);
-            if (!new_val)
-                return (0);
-            free(tmp->value);
-            tmp->value = new_val;
-            return (1);
-        }
-        tmp = tmp->next;
-    }
+	tmp = ht->array[idx];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (!tmp->value)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 
-    /* Key does not exist, create new node */
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node)
-        return (0);
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
 
-    new_node->key = strdup(key);
-    if (!new_node->key)
-    {
-        free(new_node);
-        return (0);
-    }
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
 
-    new_node->value = strdup(value);
-    if (!new_node->value)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 
-    /* Insert node at the beginning of the list (collision handling) */
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
 
-    return (1);
+	return (1);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,24 +2,37 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
-/* Structures, if any */
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key, string (must be unique in the hash table)
+ * @value: The value associated with the key
+ * @next: Pointer to the next node in the list (for collisions)
+ */
 typedef struct hash_node_s
 {
-	const char *key;
-	const char *value;
+	char *key;
+	char *value;
 	struct hash_node_s *next;
 } hash_node_t;
 
+/**
+ * struct hash_table_s - Hash table data structure
+ * @size: The size of the array
+ * @array: Array of pointers to the first node of each index
+ */
 typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
 
-/* Function prototypes */
+/* Prototypes */
+hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
 #endif /* HASH_TABLES_H */
